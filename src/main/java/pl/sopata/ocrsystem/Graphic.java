50562bad5bd492c3cfcf1f7658c0be5169c9195a
Jan943 <jansopata10@gmail.com>
2022-07-12 23:01:36 +0200

    Test Lombok

--- src/main/java/pl/sopata/ocrsystem/Graphic.java
+++ src/main/java/pl/sopata/ocrsystem/Graphic.java
@@ -1,14 +1,21 @@
 package pl.sopata.ocrsystem;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+//@NoArgsConstructor
 public class Graphic {
 
     @GeneratedValue
     @Id
+//    @Getter
+//    @Setter
     private Long id;
     private String url;
     private String content;
--- src/main/java/pl/sopata/ocrsystem/OcrApi.java
+++ src/main/java/pl/sopata/ocrsystem/OcrApi.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class OcrApi {
