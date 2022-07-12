1eaecf7161b722d070905f6c43e931ddbc11c661
Jan943 <jansopata10@gmail.com>
2022-07-13 00:30:52 +0200

    Bugfix:
    -AUTOINCREMENT on id property

--- src/main/java/pl/sopata/ocrsystem/Graphic.java
+++ src/main/java/pl/sopata/ocrsystem/Graphic.java
@@ -1,21 +1,24 @@
 package pl.sopata.ocrsystem;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 //@NoArgsConstructor
 public class Graphic {
 
-    @GeneratedValue
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    @Getter
 //    @Setter
+    @JsonIgnore
     private Long id;
     private String url;
     private String content;
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Graphic
 (
-id bigint NOT NULL,
+id bigint NOT NULL AUTO_INCREMENT,
 content varchar(255),
 url varchar(255),
 PRIMARY KEY (id)
